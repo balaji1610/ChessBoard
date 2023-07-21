1e236ce1b0d8b9876af666a4719adb8be755549c
balaji1610 <balajiece1610@gmail.com>
2023-07-22 02:22:57 +0530

    Play

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
-function App() {
+export default function App() {
   const [game, setGame] = useState(new Chess());
 
-  console.log(game.ascii(), "game");
+  useEffect(() => {
+    setGame(new Chess());
+  }, []);
+  const makeMove = (move) => {
+    game.move(move);
+    setGame(new Chess(game.fen()));
+  };
+
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0)
+      return; // exit if the game is over
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    makeMove(possibleMoves[randomIndex]);
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    console.log(sourceSquare, targetSquare, "sourceSquare, targetSquare");
+    const move = makeMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: "q", // always promote to a queen for example simplicity
+    });
+
+    // illegal move
+    if (move === null) return false;
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
   return (
     <div className="chess_Flex">
       <div className="content">
-        <Chessboard position={game.fen()} />
+        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
       </div>
     </div>
   );
 }
-
-export default App;
